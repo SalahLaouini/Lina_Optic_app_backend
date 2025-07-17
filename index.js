5340d33b4bd9027483cf6a00b543842359182e8f
SalahLaouini <linaopticlunettes@gmail.com>
2025-07-17 12:30:40 +0100

    update new url in index.js

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
       "http://localhost:5173",
       "https://lina-optic-app-frontend-khav.vercel.app",
       "https://lina-optic-app-frontend.vercel.app",
-      "https://linaoptic.com"
+      "https://linaoptic.com",
+      "https://www.linaoptic.com"
     ];
 
 // ===============================
